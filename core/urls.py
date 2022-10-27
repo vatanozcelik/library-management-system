020a7889349a129cad773627938e16d0a1c9c826
Vatan Ozcelik <vatanozcelik91@gmail.com>
2022-10-27 21:25:01 +0300

    utilizing from listCreateAPIView

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from core.views import BookList
+from core.views import ListCreateView
 
 urlpatterns = [
-    path("", BookList.as_view(), name="book-list"),
+    path("", ListCreateView.as_view(), name="book-list-create"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import (
+    ListCreateAPIView,
+)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.mixins import CreateModelMixin
 from django.http import Http404
 
 
@@ -8,6 +13,7 @@ from core.models import Book
 from core.serializers import BookSerializer
 
 
+"""
 class BookList(APIView):
 
     def get_object(self, request, pk):
@@ -40,3 +46,25 @@ class BookList(APIView):
         book = self.get_object(pk)
         book.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+"""
+
+
+class ListCreateView(ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    # def list(self, request):
+
+    #     queryset = self.get_queryset()
+    #     serializer = BookSerializer(queryset, many=True)
+    #     return Response(serializer.data)
+
+    # def create(self, request):
+
+    #     serializer = self.get_serializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
