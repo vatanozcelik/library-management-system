5363a7fd1ca2ca9f1187a1d9d8bac3ea0af4fb5c
Vatan Ozcelik <vatanozcelik91@gmail.com>
2022-10-27 10:24:07 +0300

    models and admin py modified

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from core.models import Book, BookContributor, Contributor
+from core.models import (
+    Book,
+    BookContributor,
+    Contributor,
+    UserBook,
+)
 
 
 class BookAdmin(admin.ModelAdmin):
@@ -8,6 +13,23 @@ class BookAdmin(admin.ModelAdmin):
     }
 
 
+class BookContributorAdmin(admin.ModelAdmin):
+    list_display = (
+        'book', 'contributor', 'role'
+    )
+
+
+class UserBookAdmin(admin.ModelAdmin):
+    list_display = (
+        'get_contributor'
+    )
+
+    @admin.display(ordering='book__contributor', description='Book')
+    def get_contributor(self, obj):
+        return obj.book.contributor
+
+
 admin.site.register(Book, BookAdmin)
-admin.site.register(BookContributor)
+admin.site.register(BookContributor, BookContributorAdmin)
 admin.site.register(Contributor)
+admin.site.register(UserBook)
--- core/models.py
+++ core/models.py
@@ -1,15 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Book(models.Model):
     title = models.CharField(max_length=50, blank=True, null=True)
     contributor = models.ManyToManyField(
         'Contributor', through='BookContributor')
+    user = models.ManyToManyField(User, through='UserBook')
     slug = models.SlugField()
     description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.title
+
+
+class UserBook(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
 
 
 class Contributor(models.Model):
@@ -25,7 +32,7 @@ class BookContributor(models.Model):
         CO_AUTHOR = "CO-AUTHOR", "Co-Author"
         EDITOR = "EDITOR", "Editor"
 
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
     contributor = models.ForeignKey('Contributor', on_delete=models.CASCADE)
     role = models.CharField(verbose_name="verbose name",
                             choices=ContributorName.choices, max_length=20)
